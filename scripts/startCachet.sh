9ed9100aea4419416450ad3c88a5906fb091d5c6
VeekeeFr <20480977+VeekeeFr@users.noreply.github.com>
2018-09-26 01:27:17 +0200

    [docker] added log messages during start process

--- scripts/startCachet.sh
+++ scripts/startCachet.sh
@@ -5,7 +5,9 @@ export SCRIPTPATH=`dirname $SCRIPT`
 
 if [ -f /www/scripts/hook-prestart.sh ]
 then
+	echo "Using prestart hook"
 	/www/scripts/hook-prestart.sh
 fi
 
+echo "Starting cachet process"
 /usr/local/bin/cachet_monitor -c /www/conf/cachet-monitor.yml --log=/www/log/cachet-monitor.log
