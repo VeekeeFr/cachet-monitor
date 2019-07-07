8f354d7ac51c2422d88303f1678ab45ef40b6746
VeekeeFr <20480977+VeekeeFr@users.noreply.github.com>
2019-07-07 14:50:17 +0200

    [docker] detecting error when process ends and showing some debug data

--- scripts/startCachet.sh
+++ scripts/startCachet.sh
@@ -11,3 +11,16 @@ fi
 
 echo "Starting cachet process"
 /usr/local/bin/cachet_monitor --config=/www/conf/cachet-monitor.yml --log=/www/log/cachet-monitor.log
+RETURN_CODE=$?
+
+echo "Return code: ${RETURN_CODE}"
+if [ ${RETURN_CODE} -gt 0 ]
+then
+	echo "Configuration data..."
+	cat /www/conf/cachet-monitor.yml
+
+	echo "##################################"
+
+	echo "Latest log entries..."
+	tail -20 /www/log/cachet-monitor.log
+fi
